31a6aeeeaed02eaa0963a626362598b687848a8a
obektas <oguzhan.bektas@snitechnology.net>
2022-03-15 14:12:58 +0300

    prod and local properties created

--- src/main/java/com/sha/springbootmicroservice3gateway/SpringBootMicroservice3GatewayApplication.java
+++ src/main/java/com/sha/springbootmicroservice3gateway/SpringBootMicroservice3GatewayApplication.java
@@ -2,11 +2,10 @@ package com.sha.springbootmicroservice3gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
+@PropertySource("classpath:application-${spring.profiles.active:default}.properties")
 public class SpringBootMicroservice3GatewayApplication {
 
     public static void main(String[] args) {
